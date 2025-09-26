99d0b6dceec466ae734b6cf9330fae5f2dfdbb0d
Searchcat37 <56055095+Searchcat37@users.noreply.github.com>
2025-09-26 13:14:40 -0400

    CalcGUI mod finished

--- 3 calcGUI/calcGUI.py
+++ 3 calcGUI/calcGUI.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from math import sqrt as sqr
+from math import sqrt as sqr, sin, cos, tan
 
 
 class Calculator(Frame):
@@ -35,6 +35,17 @@ class Calculator(Frame):
         if self.entry.get() == "Invalid Input":
             self.entry.delete(0,END)
         self.entry.insert(END, char)
+        self.entry.configure(state="disabled")
+
+    def add_lastname(self):
+        """
+        Inserts your last name into the Entry.
+        """
+        self.entry.configure(state="normal")
+        self.flash(self.firstname_bttn)
+        if self.entry.get() == "Invalid Input":
+            self.entry.delete(0,END)
+        self.entry.insert(END, "Kennel")
         self.entry.configure(state="disabled")
 
     def clear(self):
@@ -74,9 +85,12 @@ class Calculator(Frame):
         e = e.replace("²", "**2")
         e = e.replace("^", "**")
         e = e.replace("÷", "/")
+        e = e.replace("sin", "sin")
+        e = e.replace("cos", "cos")
+        e = e.replace("tan", "tan")
 
         try:
-            ans = eval(e)
+            ans = eval(e, {"sqr": sqr, "sin": sin, "cos": cos, "tan": tan})
         except Exception as ex:
             self.entry.delete(0,END)
             self.entry.insert(0, "Invalid Input")
@@ -126,7 +140,7 @@ class Calculator(Frame):
         master.bind("4", lambda event, char="DT", btn=self.four_bttn: self.add_chr(char, btn))
         master.bind("3", lambda event, char="3", btn=self.three_bttn: self.add_chr(char, btn))
         master.bind("2", lambda event, char="2", btn=self.two_bttn: self.add_chr(char, btn))
-        master.bind("1", lambda event, char="1", btn=self.one_bttn: self.add_chr(char, btn))
+        # master.bind("1", lambda event, char="1", btn=self.one_bttn: self.add_chr(char, btn))
         master.bind("0", lambda event, char="0", btn=self.zero_bttn: self.add_chr(char, btn))
         master.bind("*", lambda event, char="×", btn=self.mult_bttn: self.add_chr(char, btn))
         master.bind("/", lambda event, char="÷", btn=self.div_bttn: self.add_chr(char, btn))
@@ -144,8 +158,8 @@ class Calculator(Frame):
         Creates the widgets to be used in the grid.
         :return: None
         """
-        self.eq_bttn = Button(self, text="=", width=20, height=3, bg="lightgrey", command=lambda: self.calculate())
-        self.eq_bttn.grid(row=4, column=4, columnspan=2)
+        self.eq_bttn = Button(self, text="=", width=9, height=3, bg="lightgrey", command=lambda: self.calculate())
+        self.eq_bttn.grid(row=4, column=4, columnspan=1)
 
         self.ac_bttn = Button(self, text='CE', width=9, height=3, command=lambda: self.clear_all())
         self.ac_bttn.grid(row=1, column=4)
@@ -186,8 +200,8 @@ class Calculator(Frame):
         self.six_bttn = Button(self, text="6", width=9, height=3, command=lambda: self.add_chr(6))
         self.six_bttn.grid(row=2, column=2)
 
-        self.one_bttn = Button(self, text="1", width=9, height=3, command=lambda: self.add_chr(1))
-        self.one_bttn.grid(row=3, column=0)
+        self.firstname_bttn = Button(self, text="Colin", width=9, height=3, command=self.add_lastname)
+        self.firstname_bttn.grid(row=3, column=0)
 
         self.two_bttn = Button(self, text="2", width=9, height=3, command=lambda: self.add_chr(2))
         self.two_bttn.grid(row=3, column=1)
@@ -212,6 +226,15 @@ class Calculator(Frame):
 
         self.sqr_bttn = Button(self, text="^", width=9, height=3, command=lambda: self.add_chr('^'))
         self.sqr_bttn.grid(row=3, column=5)
+
+        self.sin_bttn = Button(self, text="sin", width=9, height=3, command=lambda: self.add_chr('sin('))
+        self.sin_bttn.grid(row=2, column=5)
+
+        self.cos_bttn = Button(self, text="cos", width=9, height=3, command=lambda: self.add_chr('cos('))
+        self.cos_bttn.grid(row=3, column=5)
+
+        self.tan_bttn = Button(self, text="tan", width=9, height=3, command=lambda: self.add_chr('tan('))
+        self.tan_bttn.grid(row=4, column=5)
 
 root = Tk()
 root.geometry()
